9ea6e21eefb739340795564b32b32724e82a6699
Ayushi2009 <83349308+Ayushi2009@users.noreply.github.com>
2021-07-14 18:05:48 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,37 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 30,
+          
+        }
+        this.pointB = pointB
+ this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+
+
+    attach(body){
+        this.chain.bodyA = body
+        }
+    fly(){
+        this.chain.bodyA = null
+    }
+    
+    display(){
+      
+        
+        
+       
+                if(this.chain.bodyA){
+                    var pointA = this.chain.bodyA.position;
+                    var pointB = this.pointB;
+                 strokeWeight(4);
+                      fill ("black")
+                      line(pointA.x, pointA.y, pointB.x, pointB.y);                                                               
+                }
+            }
+        }
